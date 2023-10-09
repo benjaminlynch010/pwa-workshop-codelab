5c2297386e76baba8e7cb365f84f76c8f49fcb68
Ben Lynch <bdlynch7@gmail.com>
2023-10-09 16:38:16 -0500

    finished part 2

--- service-worker.js
+++ service-worker.js
@@ -4,12 +4,7 @@ import { registerRoute } from 'workbox-routing';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { StaleWhileRevalidate } from 'workbox-strategies';
-
-
-
-
-
-
+import { offlineFallback } from 'workbox-recipes';
 
 // It starts by setting up a new Cache First caching strategy, chosen instead of a Cache Only strategy to allow for other pages to be added to the cache as needed. A name is given to it, page-cache. Workbox strategies can take a number of plugins that can affect the lifecycle of saving and retrieving content from the cache. Here, two plugins, the Cacheable Response plugin and the Expiration plugin, are used to ensure only good server responses are cached, and that each item in cache will get flushed after 30 days.
 const pageCache = new CacheFirst({
@@ -34,7 +29,6 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // Finally, a new route is registered. Any request that's a page navigation will be managed by this Cache First strategy, either pulling from the cache or the network and then caching the response.
 registerRoute(
-  
   // This route starts by determining if the type of request is a style, a script, or a worker, corresponding to CSS, JavaScript, or Web Workers. If it is, it uses a Stale While Revalidate strategy, trying to serve from the cache first, falling back to the network if not available, while trying to update the version in cache from the network if possible. Like the page strategy, this strategy will only cache good responses.
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
@@ -46,3 +40,8 @@ registerRoute(
     ],
   }),
 );
+
+// The offline fallback recipe sets up a Cache Only strategy that gets warmed with the provided fallbacks. It then sets up a Workbox default catch handler, catching any failed routing requests (if there's nothing in the cache and something can't be reached on the network), pulling the content of the relevant files from the cache and returning it as content as long as the request continues to fail.
+offlineFallback({
+  pageFallback: '/offline.html',
+});
