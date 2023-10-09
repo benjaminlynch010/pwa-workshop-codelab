7e875a2fe81a1e42fcbd0d756dde38d1989cfd5f
Ben Lynch <bdlynch7@gmail.com>
2023-10-09 16:58:58 -0500

    finished part 3

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
-/*
- Copyright 2021 Google LLC
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+import { openDB } from 'idb';
 
 // Register the service worker
 if ('serviceWorker' in navigator) {
@@ -41,16 +27,42 @@ if ('serviceWorker' in navigator) {
 }
 
 window.addEventListener('DOMContentLoaded', async () => {
+  // Here, an IndexedDB database called settings-store is created. Its version is initialized to 1 and its initialized with an object store called settings. This is the most basic kind of object store, simple key-value pairs, but more complex object stores can be created as needed. Without this initialization of an object store, there will be nowhere to put data in, so leaving this out here would be like creating a database with no tables.
+  const db = await openDB('settings-store', 1, {
+    upgrade(db) {
+      db.createObjectStore('settings');
+    },
+  });
+
   // Set up the editor
   const { Editor } = await import('./app/editor.js');
   const editor = new Editor(document.body);
+
+  // Set up night mode toggle
+  const { NightMode } = await import('./app/night-mode.js');
+  new NightMode(
+    document.querySelector('#mode'),
+    async (mode) => {
+      editor.setTheme(mode);
+      // Save the night mode setting when changed
+    },
+    // Retrieve the night mode setting on initialization
+  );
 
   // Set up the menu
   const { Menu } = await import('./app/menu.js');
   new Menu(document.querySelector('.actions'), editor);
 
+  // Save content to database on edit
+  // db is the previously opened IndexedDB database. The put method allows entries in an object store in that database to be created or updated. The first argument is the object store in the database to use, the second argument is the value to store, and the third argument is the key to save the value to if it's not clear from the value (in this case it's not as our database doesn't include specified keys). Because it's asynchronous, it's wrapped in async/await.
+  editor.onUpdate(async (content) => {
+    await db.put('settings', content, 'content');
+  });
+
   // Set the initial state in the editor
   const defaultText = `# Welcome to PWA Edit!\n\nTo leave the editing area, press the \`esc\` key, then \`tab\` or \`shift+tab\`.`;
+  
 
-  editor.setContent(defaultText);
+  // Instead of just setting the editor to the value of defaultText, it now attempts to get the content key from the settings object store in the settings-store IndexedDB database. If that value exists,, that's used. If not, the default text is used.
+  editor.setContent((await db.get('settings', 'content')) || defaultText);
 });
